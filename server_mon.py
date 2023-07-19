03ece33811c40d515bfabdf5f105e85b48c79a6d
unknown <25666053+ployt0@users.noreply.github.com>
2023-07-19 12:15:18 +0100

    fix issue

--- server_mon.py
+++ server_mon.py
@@ -26,23 +26,27 @@ def interrog_routine(err_handler: ErrorHandler, rmt_pc: dict,
                      ipv4: str, latencies: List[str]):
     ave_latency_ms = str(int(round(sum(map(float, latencies)) / len(latencies))))
     max_latency_ms = str(int(round(max(map(float, latencies)))))
-    try:
-        resp = requests.get(rmt_pc["home_page"], timeout=5, verify=rmt_pc.get("verify", True))
-    except SSLError as ssl_e:
-        err_handler.append(ssl_e)
-        resp = requests.get(rmt_pc["home_page"], timeout=5, verify=False)
-    except requests.exceptions.ConnectionError as awol_e:
-        # We failed. Not merely on TLS but the whole HTTP(S) response.
-        err_handler.append(awol_e)
-        # That will send us a whole, overly long, stack trace, later.
-        return
-    response_ms = str(int(round(1000 * resp.elapsed.total_seconds()))) \
-        if resp.ok else None
+    response_ms = None
+    status_code = None
+    if "home_page" in rmt_pc:
+        try:
+            resp = requests.get(rmt_pc["home_page"], timeout=5, verify=rmt_pc.get("verify", True))
+        except SSLError as ssl_e:
+            err_handler.append(ssl_e)
+            resp = requests.get(rmt_pc["home_page"], timeout=5, verify=False)
+        except requests.exceptions.ConnectionError as awol_e:
+            # We failed. Not merely on TLS but the whole HTTP(S) response.
+            err_handler.append(awol_e)
+            # That will send us a whole, overly long, stack trace, later.
+            return
+        status_code = resp.status_code
+        if resp.ok:
+            response_ms = str(int(round(1000 * resp.elapsed.total_seconds())))
     ssh_interrogator = SSHInterrogator(err_handler)
     ssh_interrogator.do_queries(rmt_pc)
     result_holder.append(CheckResult(
         result_holder.time.strftime(DAY_TIME_FMT), ipv4, ave_latency_ms,
-        max_latency_ms, response_ms, str(resp.status_code),
+        max_latency_ms, response_ms, str(status_code),
         ssh_interrogator.mem_avail, ssh_interrogator.swap_free,
         ssh_interrogator.disk_avail, ssh_interrogator.last_boot,
         ssh_interrogator.ports, ssh_interrogator.ssh_peers
